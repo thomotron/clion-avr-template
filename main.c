d0032e5ee2553adfb7ab3d6d2982be790bb6babb
Thomas Wade <gigatech55@gmail.com>
2019-10-13 12:09:13 +1030

    Strip back the template

--- main.c
+++ main.c
@@ -1,19 +1,6 @@
 #include <avr/io.h>
-#include <util/delay.h>
-#include "easyavr.h"
 
-#define LED_PORT PORTD
-#define LED_PIN 0
-#define LED_DDR DDRD
-
-int main(void)
+int main()
 {
-    PIN_AS_OUTPUT(LED_DDR, LED_PIN);
-
-    while (1) {
-        PIN_ON(LED_PORT, LED_PIN);
-        _delay_ms(100);
-        PIN_OFF(LED_PORT, LED_PIN);
-        _delay_ms(100);
-    }
+    
 }
